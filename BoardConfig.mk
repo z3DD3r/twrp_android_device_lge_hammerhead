66115c51d56342907d42d0ed19bebfbe9114fe2f
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2021-03-29 10:50:17 +0300

    hammerhead: Bump version to HH.R.12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := HH.R.11
+TW_DEVICE_VERSION := HH.R.12
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_NTFS_3G := true
